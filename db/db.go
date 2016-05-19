65b494ee4c030d683c22d0f9e12cbe95ad8739db
YannickDot <yannick.levif@gmail.com>
2016-05-19 16:53:20 +0200

    heroku

--- db/db.go
+++ db/db.go
@@ -9,7 +9,9 @@ import (
 )
 
 func InitRedisQueue() *resque.RedisEnqueuer {
-  client := redis.New("tcp:127.0.0.1:6379", 0, "") // Create new Redis client to use for enqueuing
+  LOCAL_URL := "tcp:127.0.0.1:6379"
+  NET_URL := "redis://h:p42fot0r6meaf4199koelefk9b9@ec2-54-217-222-237.eu-west-1.compute.amazonaws.com:12429"
+  client := redis.New(NET_URL, 0, "") // Create new Redis client to use for enqueuing
   enqueuer := resque.NewRedisEnqueuer("godis", client) // Create enqueuer instance
   return enqueuer
 }
